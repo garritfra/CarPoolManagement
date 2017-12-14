b78609f4218c5423bb0c70dc64e49e063a241ecd
Franke, Garrit <garrit.franke@ckc.de>
2017-12-14 09:36:03 +0100

    add import/export class

---
+++ src/de/ckc/franke/ausbildung/io/Data.java
@@ -0,0 +1,16 @@
+package de.ckc.franke.ausbildung.io;
+
+import java.util.LinkedList;
+
+import de.ckc.franke.ausbildung.model.Vehicle;
+
+public class Data {
+
+	/**
+	 * Parse a string to csv
+	 */
+	public static void toCSV(LinkedList<Vehicle> vehicleList) {
+		
+	}
+	
+}
