595897ae8e0d5eead69525a7261177260c5373f6
Franke, Garrit <garrit.franke@ckc.de>
2017-12-15 13:45:19 +0100

    add class Enums

---
+++ src/de/ckc/franke/ausbildung/util/Enums.java
@@ -0,0 +1,15 @@
+package de.ckc.franke.ausbildung.util;
+
+public class Enums {
+
+	public enum Error {
+		invalidInteger,
+		invalidInput,
+		noReservations,
+		invalidID,
+		idNotFound,
+		noVehicles,
+		invalidDate
+	}
+	
+}
