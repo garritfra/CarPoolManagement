7dc3d5920172d3aff54327ce660b41c0cac9c117
Franke, Garrit <garrit.franke@ckc.de>
2017-12-21 08:46:54 +0100

    delete database methods -> see database branch

--- src/de/ckc/franke/ausbildung/Main.java
+++ src/de/ckc/franke/ausbildung/Main.java
@@ -1,10 +1,8 @@
 package de.ckc.franke.ausbildung;
 
-import java.sql.Connection;
 import java.util.LinkedList;
 
 import de.ckc.franke.ausbildung.io.Data;
-import de.ckc.franke.ausbildung.io.DB;
 import de.ckc.franke.ausbildung.model.Vehicle;
 import de.ckc.franke.ausbildung.util.Console;
 
@@ -20,16 +18,6 @@ public class Main {
 
 		// clear screen before program runs
 		Console.clear();
-
-		String path = "jdbc:sqlite:E:\\Daten_Garrit_Franke\\Datenbanken\\";
-		String fileName = "Vehicles";
-		//String absolutePath = path + fileName;
-//		Connection conn;
-		
-		
-		DB.createNewDatabase(fileName);
-//		conn = DB.connect();
-		DB.selectAll();
 
 		// start program
 
